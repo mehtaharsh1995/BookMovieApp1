43433810d3b3bd7ec390ee39e4993b39bd1d77e3
mehtaharsh1995 <mehta.harsh.harsh@gmail.com>
2021-08-22 12:17:26 +0530

    Added HomePage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
 import Header from './common/header/Header'
+import Controller from './screens/Controller';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
